f16f15c47e56227df43799459eab0555ec724d2e
den.postika <den.postika@gmail.com>
2016-11-26 13:01:23 +0200

    Minor

---
+++ app/Http/Controllers/TrackerController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use DB;
+use YouTrack\Connection;
+
+
+class TrackerController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+      	$user = Auth::user();
+
+        $youtrack =  new Connection(
+            $user->youtrack_url,
+            $user->youtrack_email,
+            $user->youtrack_password
+          );
+
+        return view('tracker' , array( 'user' => $user ) );
+    }
+}
