407a9b684647702d0cd42ac5af77bca9cc1d0f26
Vladimir.K <aonoams@gmail.com>
2016-11-26 16:07:55 +0200

    aazaza

--- resources/views/isueses.blade.php
+++ resources/views/isueses.blade.php
@@ -11,20 +11,20 @@
                 <div class="panel-body">
                   <table class="table table-striped table-hover">
                     <thead>
-                      <th>#</th>
-                      <th>Название задачи</th>
-                      <th>Тип задачи</th>
-                      <th>Приоритет</th>
-                      <th>Estimation</th>
+                      <th class="colunm0">#</th>
+                      <th class="colunm1">Название задачи</th>
+                      <th class="colunm2">Тип задачи</th>
+                      <th class="colunm3">Приоритет</th>
+                      <th class="colunm4">Estimation</th>
                     </thead>
                     <tbody>
                      @foreach( $isues as $n => $isue )
                       <tr>
-                        <td>{{ ++$n }}</td>
-                        <td><a href="/issue?id={{ $isue->id }}">{{ $isue->summary }}</a></td>
-                        <td>{{ $isue->Type }}</td>
-                        <td>{{ $isue->Priority }}</td>
-                        <td>{{ ($isue->Estimation) ? ($isue->Estimation / 60) . ' ч.' : '?' }}</td>
+                        <td class="colunm0">{{ ++$n }}</td>
+                        <td class="colunm1"><a href="/issue?id={{ $isue->id }}">{{ $isue->summary }}</a></td>
+                        <td class="colunm2">{{ $isue->Type }}</td>
+                        <td class="colunm3">{{ $isue->Priority }}</td>
+                        <td class="colunm4">{{ ($isue->Estimation) ? ($isue->Estimation / 60) . ' ч.' : '?' }}</td>
                       </tr>
                      @endforeach
                     </tbody>
