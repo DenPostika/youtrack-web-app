e94e6404ec25e70b2cfe245d28192ebe8a194bd4
den.postika <den.postika@gmail.com>
2016-11-26 14:00:34 +0200

    Add page tracker

---
+++ resources/views/tracker.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@extends('layouts.sideMenu')
+
+@section('content')
+<div class="col-xs-9">
+    <div class="row">
+        <div class="">
+            <div class="panel panel-default">
+                <div class="panel-heading">Трекер</div>
+                <div class="panel-body">
+                  <span id="time">0ч 20м</span>
+                  <a id="action">Начать</a>
+                  <textarea id="what_action" placeholder="Что сейчас делаешь?"></textarea>
+                  <table class="table table-hover">
+                    <thead>
+                      <th>#</th>
+                      <th>Дата</th>
+                      <th>Задание</th>
+                      <th>Потраченое время</th>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <th>2016-11-12</th>
+                        <th>Выложить модуль Таксономии WP</th>
+                        <th>2ч 30м</th>
+                      </tr>
+                      <tr>
+                        <td>1</td>
+                        <th>2016-11-12</th>
+                        <th>Выложить модуль Таксономии WP</th>
+                        <th>2ч 30м</th>
+                      </tr>
+                      <tr>
+                        <td>1</td>
+                        <th>2016-11-12</th>
+                        <th>Выложить модуль Таксономии WP</th>
+                        <th>2ч 30м</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
