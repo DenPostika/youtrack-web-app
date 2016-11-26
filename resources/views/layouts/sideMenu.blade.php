f6a4e726e08832f2769130c6c2693a18f6811dc5
Vladimir.K <aonoams@gmail.com>
2016-11-26 15:22:00 +0200

    style for raq

--- resources/views/layouts/sideMenu.blade.php
+++ resources/views/layouts/sideMenu.blade.php
@@ -1,9 +1,9 @@
 @section('side_menu')
-<div class="col-xs-3">
-  <nav class="navbar navbar-default menu-raq">
+<div class="col-xs-2">
+  <nav class="menu-raq">
       <div class="">
           <div class="header-raq clearfix">
-
+              <div class="circle"></div>
               <!-- Collapsed Hamburger -->
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                   <span class="sr-only">Toggle Navigation</span>
@@ -11,49 +11,64 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
               </button>
-
+              <ul class="nav navbar-raq">
               <!-- Branding Image -->
-              <a class="navbar-brand" href="{{ url('/') }}">
-                  {{ config('app.name', 'Laravel') }}
-              </a>
+              <!-- Authentication Links -->
+              @if (Auth::guest())
+                  <li><a href="{{ url('/login') }}">Login</a></li>
+                  <li><a href="{{ url('/register') }}">Register</a></li>
+              @else
+                  <li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+
+                      <ul class="dropdown-menu" role="menu">
+                          <li>
+                              <a href="{{ url('/logout') }}"
+                                  onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                                  Logout
+                              </a>
+
+                              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                  {{ csrf_field() }}
+                              </form>
+                          </li>
+                      </ul>
+                  </li>
+              @endif
+              </ul>
           </div>
 
           <div class="collapse navbar-collapse" id="app-navbar-collapse">
               <!-- Left Side Of Navbar -->
-              <ul class="nav navbar-nav">
-                  &nbsp;
+              <ul class="nav nav-pils nav-stacked navbar-raq">
+                <li class="active">
+                    <a class="" href="{{ url('/') }}">
+                      <i class="material-icons">dashboard</i>
+                      dashboard
+                    </a>
+                </li>
               </ul>
 
               <!-- Right Side Of Navbar -->
               <ul class="nav nav-pils nav-stacked navbar-raq">
-                  <li class="active"><a href="{{ url('/tracker') }}">Трекер</a></li>
-                  <li><a href="{{ url('/projects') }}">Проекты</a></li>
-                  <li><a href="{{ url('/settings') }}">Настройки</a></li>
-                  <!-- Authentication Links -->
-                  @if (Auth::guest())
-                      <li><a href="{{ url('/login') }}">Login</a></li>
-                      <li><a href="{{ url('/register') }}">Register</a></li>
-                  @else
-                      <li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                              {{ Auth::user()->name }} <span class="caret"></span>
-                          </a>
 
-                          <ul class="dropdown-menu" role="menu">
-                              <li>
-                                  <a href="{{ url('/logout') }}"
-                                      onclick="event.preventDefault();
-                                               document.getElementById('logout-form').submit();">
-                                      Logout
-                                  </a>
+                  <li>
+                    <a href="{{ url('/tracker') }}">
+                    <i class="material-icons">timer</i>
+                    Трекер</a></li>
+                  <li><a href="{{ url('/projects') }}">
+                    <i class="material-icons">featured_play_list</i>
+                    Проекты</a></li>
+                  <li>
+                    <a href="{{ url('/settings') }}">
+                    <i class="material-icons">settings</i>
+                    Настройки</a></li>
 
-                                  <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                      {{ csrf_field() }}
-                                  </form>
-                              </li>
-                          </ul>
-                      </li>
-                  @endif
+
+
               </ul>
           </div>
       </div>
